ca4dc5eaaeda53ed138078a2d2806484c568ec52
Patti Shin <pattishin@users.noreply.github.com>
2015-01-03 23:10:13 -0500

    Filling out mode collection, adding new menu

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
+var Settings = require('settings');
 var ajax = require('ajax');
+var modeCollection = require('modeCollection');
+
 
 /**
  * @class n3rdtones
@@ -8,13 +11,69 @@ var ajax = require('ajax');
  */
 
 var modes = [
-	{ title: "Ionian", subtitle: "C-D-E-F-G-A-B-C" },
-	{ title: "Dorian", subtitle: "D-E-F-G-A-B-C-D" },
-	{ title: "Phrygian", subtitle: "E-F-G-A-B-C-D-E" },
-	{ title: "Lydian", subtitle: "F-G-A-B-C-D-E-F" },
-	{ title: "Mixolydian", subtitle: "G-A-B-C-D-E-F-G" },
-	{ title: "Aeolian", subtitle: "A-B-C-D-E-F-G-A" },
-	{ title: "Locrian", subtitle: "B-C-D-E-F-G-A-B" }
+	{ 
+		title: "Ionian", 
+		subtitle: "C-D-E-F-G-A-B-C",
+		fretboard: [
+			{ A: [3,5] },
+			{ D: [2,3,5] },
+			{ G: [2,4,5] }
+		]
+	},
+	{ 
+		title: "Dorian", 
+		subtitle: "D-E-F-G-A-B-C-D", 
+		fretboard: [
+			{ A: [3,5] },
+			{ D: [2,3,5] },
+			{ G: [2,4,5] }
+		]
+	},
+	{ 
+		title: "Phrygian", 
+		subtitle: "E-F-G-A-B-C-D-E", 
+		fretboard: [
+			{ A: [3,5] },
+			{ D: [2,3,5] },
+			{ G: [2,4,5] }
+		]
+	},
+	{ 
+		title: "Lydian", 
+		subtitle: "F-G-A-B-C-D-E-F", 
+		fretboard: [
+			{ A: [3,5] },
+			{ D: [2,3,5] },
+			{ G: [2,4,5] }
+		]
+	},
+	{ 
+		title: "Mixolydian", 
+		subtitle: "G-A-B-C-D-E-F-G",
+		fretboard: [
+			{ A: [3,5] },
+			{ D: [2,3,5] },
+			{ G: [2,4,5] }
+		]
+	},
+	{ 
+		title: "Aeolian", 
+		subtitle: "A-B-C-D-E-F-G-A", 
+		fretboard: [
+			{ A: [3,5] },
+			{ D: [2,3,5] },
+			{ G: [2,4,5] }
+		]
+	},
+	{ 
+		title: "Locrian", 
+		subtitle: "B-C-D-E-F-G-A-B", 
+		fretboard: [
+			{ A: [3,5] },
+			{ D: [2,3,5] },
+			{ G: [2,4,5] }
+		]
+	}
 ];
 
 var modeList = new UI.Menu({
@@ -28,24 +87,32 @@ modeList.show();
 
 modeList.on('select', function(event) {
 
-	var window = new UI.Window(); 
-	var bgRect = new UI.Rect({
-		position: new Vector2(10, 20),
-		size: new Vector2(124, 60),
-		backgroundColor: 'white'
-	 });
+	var window = new UI.Window(),
+		bgRect = new UI.Rect({
+			position: new Vector2(10, 30),
+			size: new Vector2(124, 60),
+			backgroundColor: 'white'
+	 	}),
+		index = event.itemIndex;
 
  	window.add(bgRect);
 
-  	var detail = new UI.Text({
-		position: new Vector2(0, 0),
+  	var modeTitle = new UI.Text({
+		position: new Vector2(10, 10),
  		size: new Vector2(144, 168),
 		font: 'gothic-18-bold',
-    	text: modes[event.itemIndex].title,
+    	text: modes[item].title + " Mode",
  	});
+  	/*var modeDetail = new UI.Text({
+		position: new Vector2(10, 40),
+ 		size: new Vector2(144, 168),
+		font: 'gothic-18-bold',
+    	text: modes[item].subtitle,
+ 	});*/
+	
+	
+  window.add(modeTitle);
+  //window.add(modeDetail);
 
-  window.add(detail);
-
-  // Show the new Card
   window.show();
 });
---
+++ src/modeCollection.js
@@ -0,0 +1,11 @@
+var modes = [
+	{ title: "Ionian", subtitle: "C-D-E-F-G-A-B-C" },
+	{ title: "Dorian", subtitle: "D-E-F-G-A-B-C-D" },
+	{ title: "Phrygian", subtitle: "E-F-G-A-B-C-D-E" },
+	{ title: "Lydian", subtitle: "F-G-A-B-C-D-E-F" },
+	{ title: "Mixolydian", subtitle: "G-A-B-C-D-E-F-G" },
+	{ title: "Aeolian", subtitle: "A-B-C-D-E-F-G-A" },
+	{ title: "Locrian", subtitle: "B-C-D-E-F-G-A-B" }
+];
+
+module.export =  modes;
